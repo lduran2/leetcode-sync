5d4075b51e57d82af6ea7e88e738d21734ca8041
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-05-28 02:28:40 +0000

    Sync LeetCode submission - Rank Scores (mysql)

--- problems/rank_scores/solution.sql
+++ problems/rank_scores/solution.sql
@@ -1,43 +1,40 @@
 # Write your MySQL query statement below
 
-# calculate the rank
+# sort the scores
 SELECT
-        score,
-        # cast rank as integer
-        (
-            SELECT CAST(
-                (
-                    CASE
-                        # if same score, reuse the rank
-                        WHEN (score = score_lag) THEN @i_rank
-                        # otherwise if new score,
-                        ELSE
-                            # increment the rank
-                            (@i_rank := @i_rank + 1)
-                    END
-                )
-            AS unsigned)
-        ) AS 'rank'
+        Scores.score AS score,
+        ScoreRankings.rank
     FROM
+        Scores
+        LEFT JOIN
         (
-            # add the previous score (or lag) to each score
             SELECT
                     score,
-                    lag(score, 1, null) OVER () AS score_lag
+                    # cast rank as integer
+                    (
+                        SELECT CAST(
+                            (@i_rank := @i_rank + 1)
+                        AS unsigned)
+                    ) AS 'rank'
                 FROM
                     (
                         # sort the scores
                         SELECT score
                             FROM
                                 Scores
+                            # combine equal scores
+                            GROUP BY score
                             # in order of score descending
                             ORDER BY -score
-                    ) AS SortedScores
-        ) AS ScoresWithLag
+                    ) AS UniqueScores
 
-        # initialize the ranks
-        # and cross multiply onto each row
-        CROSS JOIN (
-            SELECT @i_rank := 0 as INIT_RANK
-        ) As InitRank
+                    # initialize the ranks
+                    # and cross multiply onto each row
+                    CROSS JOIN (
+                        SELECT @i_rank := 0 AS INIT_RANK
+                    ) AS InitRank
+        ) AS ScoreRankings
+        ON Scores.score = ScoreRankings.score
+    # in order of score descending
+    ORDER BY -Scores.score
 ;
